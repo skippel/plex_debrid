722d613f5eea8563269262ac7c36734e8ca3201e
skippel <nicky_skippel@hotmail.com>
2025-02-10 15:31:48 +0100

    Update ui_settings.py

--- ui/ui_settings.py
+++ ui/ui_settings.py
@@ -1,5 +1,5 @@
-version = ['3.00', "Settings compatible update", []]
+version = ['3.01', "Settings compatible update", []]
 run_directly = "true"
 debug = "false"
 log = "false"
-loop_interval_seconds = 60
+loop_interval_seconds = 30
